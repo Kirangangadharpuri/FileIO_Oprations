0042a0895af6ada00485aff38b78c575c0793736
Kiran <kiranpuri532@gmail.com>
2023-07-24 19:02:57 +0530

    [Kiran]Add\ check file exist or note

---
+++ FileIOOperation/FileIOOperation/File_Operation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIOOperation
+{
+    public class File_Operation
+    {
+        string path = @"C:\Users\HP\Desktop\RFP-288\FileIO\FileIO_Problem\FileIO_Oprations\FileIOOperation\FileIOOperation\Files\Kiran.txt";
+        public void File_Exist()
+        {
+            if(File.Exists(path))
+            {
+                Console.WriteLine("The given file exist");
+            }
+            else
+            {
+                Console.WriteLine("The given file does not exist");
+            }
+        }
+       
+    }
+}
---
+++ FileIOOperation/FileIOOperation/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIOOperation
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                File_Operation file_Operation = new File_Operation();
+
+                Console.WriteLine("1-Check file exist or note");
+                Console.WriteLine("CHOOSE ABOVE OPTION");
+                int option=Convert.ToInt32(Console.ReadLine());
+                bool check = true;
+                switch (option)
+                {
+                    case 1:
+                        file_Operation.File_Exist();
+                        break;
+                    
+                }
+            }
+        }
+    }
+}
