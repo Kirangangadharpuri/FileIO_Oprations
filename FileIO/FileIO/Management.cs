81ae1bc345122090661d596f5f9655c16e7f0286
Kiran <kiranpuri532@gmail.com>
2023-07-26 19:25:15 +0530

    [Kiran]Add:\ performed all file operations using linq

---
+++ FileIO/FileIO/Management.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIO
+{
+    public class Management
+    {
+        public void TopThreeRecords(List<ProductReview> products)
+        {
+            var record = (from product in products orderby product.Rating descending select product).Take(3);
+            foreach (var item in record)
+            {
+                Console.WriteLine(item.ProductId + " " + item.UserId + " " + item.Rating + " " + item.Review + " " + item.IsLike);
+            }
+        }
+        public void PruductId_Rating(List<ProductReview> products)
+        {
+            var record = from product in products where (product.ProductId == 1 || product.ProductId == 4 || product.ProductId == 9) && product.Rating > 3 select product;
+            foreach (var item in record)
+            {
+                Console.WriteLine(item.ProductId + " " + item.UserId + " " + item.Rating + " " + item.Review + " " + item.IsLike);
+            }
+        }
+        public void Print_ProductId_Review(List<ProductReview> products)
+        {
+            var record = from product in products select new { product.ProductId, product.Review };
+            foreach (var item in record)
+            {
+                Console.WriteLine(item.ProductId + " " + item.Review);
+            }
+        }
+        public void RetiveProductId_UserId_Review(List<ProductReview> products)
+        {
+            var record = from product in products select new { product.ProductId, product.UserId, product.Review };
+            foreach (var item in record)
+            {
+                Console.WriteLine(item.ProductId + " " + item.UserId + " " + item.Review);
+            }
+        }
+        public void SkipTopFive(List<ProductReview> products)
+        {
+            var record = (from product in products select product).Skip(5);
+            foreach (var item in record)
+            {
+                Console.WriteLine(item.ProductId + " " + item.UserId + " " + item.Rating + " " + item.Review + " " + item.IsLike);
+            }
+        }
+        public void CoundByRating(List<ProductReview> products)
+        {
+            var record = products.GroupBy(x => x.Rating).Select(x => new { Rating = x.Key, Count = x.Count() });
+            foreach (var item in record)
+            {
+                Console.WriteLine(item.Rating + " " + item.Count);
+            }
+        }
+    }
+}
---
+++ FileIO/FileIO/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIO
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Management management = new Management();
+            List<ProductReview> list = new List<ProductReview>()
+            {
+                new ProductReview() { ProductId = 1, UserId = 101, Rating = 5, Review = "Very Good", IsLike = true },
+                new ProductReview() { ProductId = 2, UserId = 102, Rating = 4, Review = " Good", IsLike = true },
+                new ProductReview() { ProductId = 3, UserId = 103, Rating = 3, Review = "Average", IsLike = true },
+                new ProductReview() { ProductId = 4, UserId = 104, Rating = 2, Review = "Poor", IsLike = true },
+                new ProductReview() { ProductId = 5, UserId = 105, Rating = 1, Review = "Bad", IsLike = true },
+                new ProductReview() { ProductId = 6, UserId = 106, Rating = 1, Review = "Bad", IsLike = true },
+                new ProductReview() { ProductId = 7, UserId = 107, Rating = 2, Review = "Poor", IsLike = true },
+                new ProductReview() { ProductId = 8, UserId = 108, Rating = 4, Review = "Good", IsLike = true },
+                new ProductReview() { ProductId = 9, UserId = 109, Rating = 3, Review = "Average", IsLike = true },
+                new ProductReview() { ProductId = 10, UserId = 110, Rating = 5, Review = "Very Good", IsLike = true }
+            };
+            while(true)
+            {
+                Console.WriteLine("1- Select top 3 records");
+                Console.WriteLine("2- Retrive deatails of ProductId =1,4,9 and rating >3");
+                Console.WriteLine("3- print only ProductId and Review");
+                Console.WriteLine("4- Retrive productId rating userId");
+                Console.WriteLine("5-Skip top 5 records");
+                Console.WriteLine("6- find total Count");
+                Console.WriteLine("Choose above option");
+                int option=Convert.ToInt32(Console.ReadLine());
+                switch(option)
+                {
+                    case 1:
+                        management.TopThreeRecords(list);
+                        break;
+                    case 2:
+                        management.PruductId_Rating(list);
+                        break;
+                    case 3:
+                        management.Print_ProductId_Review(list);
+                        break;
+                    case 4:
+                        management.RetiveProductId_UserId_Review(list);
+                        break;
+                    case 5:
+                        management.SkipTopFive(list); 
+                        break;
+                    case 6:
+                        management.CoundByRating(list);
+                        break;
+                }
+            }
+        }
+    }
+}
